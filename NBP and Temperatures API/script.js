298d5832b15451220878f8986ed7c2a1f7d0002e
kacper26 <57824352+kacper26@users.noreply.github.com>
2020-01-21 18:17:47 +0100

    Final result, changes done

---
+++ NBP and Temperatures API/script.js
@@ -0,0 +1,33 @@
+function getData(url, dest, parser) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            dest.data = parser(xhr.responseText);
+            console.log(`${url} downloaded`);
+        }
+    };
+    xhr.send(null);
+}
+
+function getSingleCurrency(currency, startDate, endDate, dest) {
+    const url = `http://api.nbp.pl/api/exchangerates/rates/c/${currency}/${startDate}/${endDate}`;
+    getData(url, dest, (x) => JSON.parse(x));
+}
+
+const tempsURL = 'https://www.ncdc.noaa.gov/cag/global/time-series/globe/land_ocean/ytd/12/1880-2019/data.json';
+const A_URL = 'http://api.nbp.pl/api/exchangerates/tables/A';
+const B_URL = 'http://api.nbp.pl/api/exchangerates/tables/B';
+const C_URL = 'http://api.nbp.pl/api/exchangerates/tables/C';
+
+var temperatures = {};
+var tableA = {}, tableB = {}, tableC = {};
+var singleCurrencyRates = {};
+
+$(document).ready(function() {
+    getData(tempsURL, temperatures, (x) => JSON.parse(x)['data']);
+    getData(A_URL, tableA, (x) => JSON.parse(x)[0]);
+    getData(B_URL, tableB, (x) => JSON.parse(x)[0]);
+    getData(C_URL, tableC, (x) => JSON.parse(x)[0]);
+    getSingleCurrency('eur', '2020-01-01', '2020-01-21', singleCurrencyRates);
+});
