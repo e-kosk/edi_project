0ff34ceef29f041b77224cd7d2bb01074d9e41a9
Eryk Kosk <eryk.kosk@wp.pl>
2020-01-24 23:42:21 +0100

    changed fetch to xmlhttprequest, added some styling, added jokes, added key J functionality, added third chart

--- project/new_script.js
+++ project/new_script.js
@@ -1,81 +1,127 @@
+let req = new XMLHttpRequest();
+let url_nbp_1 = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/2020-01-01/2020-01-22/?format=json";
+let url_nbp_2 = "http://api.nbp.pl/api/exchangerates/rates/a/eur/2020-01-01/2020-01-22/?format=json";
+let url_joke = 'https://official-joke-api.appspot.com/random_joke';
+
+let getData = function (url) {
+    req.open('GET', url, false);
+    req.send(null);
+    let res;
+    if (req.status === 200) {
+        return JSON.parse(req.responseText);
+    } else {
+        return null;
+    }
+};
+
+let writeJoke = function (question_div, answer_div) {
+    let res_joke = getData(url_joke);
+    question_div.text(res_joke.setup);
+    answer_div.text(res_joke.punchline);
+};
+
+let drawChart = function(type, canvas, labels, datasets) {
+
+    let CHART = document.getElementById(canvas).getContext('2d');
+
+    let myChart = new Chart(CHART, {
+        type: type,
+        data: {
+            labels: labels,
+            datasets: datasets
+        },
+        options: {
+            legend: {
+                labels: {
+                    fontColor: "white",
+                    fontSize: 18
+                }
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        fontColor: "white",
+                        fontSize: 18,
+                        beginAtZero: false
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontColor: "white",
+                        fontSize: 14,
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+};
+
+
+
 $(document).ready(function () {
 
-    fetch("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2020-01-01/2020-01-22/?format=json").then(response => response.json()).then(response => {
+    // NBP CHARTS
 
-        let r = response.rates;
-        let mids = [];
-        let dates = [];
+    let res_nbp_1 = getData(url_nbp_1);
+    let r1 = res_nbp_1.rates;
 
-        $.each(r, function (index, object) {
-            mids.push(object.mid);
-            dates.push(object.effectiveDate);
-        });
+    let res_nbp_2 = getData(url_nbp_2);
+    let r2 = res_nbp_2.rates;
 
-        drawChart1('GBP rates','line', 'canvas-chart-1', dates, mids, 'brown', 'green', true)
+    let labels_1 = [];
+    let labels_2 = [];
+
+    let dataset_1 = {
+        label: 'GBP rates',
+        data: [],
+        fill: false,
+        backgroundColor: '#ffd866',
+        borderColor: '#8a7c34',
+        borderWidth: 1
+    };
+
+    let dataset_2 = {
+        label: 'EUR rates',
+        data: [],
+        fill: false,
+        backgroundColor: '#146ccc',
+        borderColor: '#162351',
+        borderWidth: 1
+    };
+
+    $.each(r1, function (index, object) {
+        dataset_1.data.push(object.mid);
+        labels_1.push(object.effectiveDate);
     });
 
-    fetch("http://api.nbp.pl/api/exchangerates/rates/a/eur/2020-01-01/2020-01-22/?format=json").then(response => response.json()).then(response => {
-
-        let r = response.rates;
-        let mids = [];
-        let dates = [];
-
-        $.each(r, function (index, object) {
-            mids.push(object.mid);
-            dates.push(object.effectiveDate);
-        });
-
-        drawChart2('EUR rates', 'bar','canvas-chart-2', dates, mids, 'blue', 'black', false, )
+    $.each(r2, function (index, object) {
+        dataset_2.data.push(object.mid);
+        labels_2.push(object.effectiveDate);
     });
-    
 
-    let drawChart1 = function(label, type, canvas, labels, values) {
+    drawChart('bar', 'canvas-chart-1', labels_1,[dataset_1]);
+    drawChart('bar', 'canvas-chart-2', labels_2,[dataset_2]);
+    drawChart('bar', 'canvas-chart-3', labels_1,[dataset_1, dataset_2]);
 
-        let CHART = document.getElementById(canvas).getContext('2d');
+    // END OF NBP CHARTS
 
-        let myChart = new Chart(CHART, {
-            type: type,
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: label,
-                    data: values,
-                    fill: false,
-                    backgroundColor: 'black',
-                    borderColor: 'red',
-                    borderWidth: 1
-            
-                }]
-            }
-        });
-        
 
-    }
-    let drawChart2 = function(label, type, canvas, labels, values) {
+    // GITHUB JOKES
 
-        let CHART = document.getElementById(canvas).getContext('2d');
+    writeJoke($('#joke-question'), $('#joke-answer'));
 
-        let myChart = new Chart(CHART, {
-            type: type,
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: label,
-                    data: values,
-                    fill: false,
-                    backgroundColor: 'blue',
-                    borderColor: 'black',
-                    borderWidth: 1
-            
-                }]
-            }
-        });
-    } 
+    $(function() {
+       $(window).keypress(function(e) {
+           var ev = e || window.event;
+           var key = ev.keyCode || ev.which;
+           //do stuff with "key" here...
+           if (key == 106) {
+               writeJoke($('#joke-question'), $('#joke-answer'));
+           }
+       });
+    });
+
+    // END OF GITHUB JOKES
+
 });
-
-
-
-
-
-
-
