f930c7b1d8062d19bd075eab9a856ad2ddc137b9
Eryk Kosk <eryk.kosk@wp.pl>
2020-01-25 12:05:05 +0100

    improved footer section, added weather info

--- project/new_script.js
+++ project/new_script.js
@@ -1,8 +1,11 @@
+// variables and urls
 let req = new XMLHttpRequest();
 let url_nbp_1 = "http://api.nbp.pl/api/exchangerates/rates/a/gbp/2020-01-01/2020-01-22/?format=json";
 let url_nbp_2 = "http://api.nbp.pl/api/exchangerates/rates/a/eur/2020-01-01/2020-01-22/?format=json";
 let url_joke = 'https://official-joke-api.appspot.com/random_joke';
+let url_weather = 'https://api.openweathermap.org/data/2.5/weather?q=Krakow,pl&units=metric&appid=bbc670541580546559ac91314174503c';
 
+// functions
 let getData = function (url) {
     req.open('GET', url, false);
     req.send(null);
@@ -14,11 +17,18 @@ let getData = function (url) {
     }
 };
 
-let writeJoke = function (question_div, answer_div) {
-    let res_joke = getData(url_joke);
-    question_div.text(res_joke.setup);
-    answer_div.text(res_joke.punchline);
+let writeJoke = function (q, a, question_div, answer_div) {
+    question_div.text(q);
+    answer_div.text(a);
 };
+
+
+
+// let writeJoke = function (q, a question_div, answer_div) {
+//     let res_joke = getData(url_joke);
+//     question_div.text(res_joke.setup);
+//     answer_div.text(res_joke.punchline);
+// };
 
 let drawChart = function(type, canvas, labels, datasets) {
 
@@ -59,69 +69,91 @@ let drawChart = function(type, canvas, labels, datasets) {
 
 
 
+// GETTING DATA FOR CHARTS
+let res_nbp_1 = getData(url_nbp_1);
+let r1 = res_nbp_1.rates;
+
+let res_nbp_2 = getData(url_nbp_2);
+let r2 = res_nbp_2.rates;
+
+let labels_1 = [];
+let labels_2 = [];
+
+let dataset_1 = {
+    label: 'GBP rates',
+    data: [],
+    fill: false,
+    backgroundColor: '#ffd866',
+    borderColor: '#8a7c34',
+    borderWidth: 1
+};
+
+let dataset_2 = {
+    label: 'EUR rates',
+    data: [],
+    fill: false,
+    backgroundColor: '#146ccc',
+    borderColor: '#162351',
+    borderWidth: 1
+};
+
+$.each(r1, function (index, object) {
+    dataset_1.data.push(object.mid);
+    labels_1.push(object.effectiveDate);
+});
+
+$.each(r2, function (index, object) {
+    dataset_2.data.push(object.mid);
+    labels_2.push(object.effectiveDate);
+});
+// END OF GETTING DATA FOR CHARTS
+
+
+// GETTING DATA FOR FIRST JOKE
+let res_joke = getData(url_joke);
+let joke_q = res_joke.setup;
+let joke_a = res_joke.punchline;
+// END OF GETTING DATA FOR FIRST JOKE
+
+
+// GETTING WEATHER DATA
+let res_weather = getData(url_weather);
+// END OF GETTING WEATHER DATA
+
+
+
 $(document).ready(function () {
 
-    // NBP CHARTS
+    console.log(res_weather.main.temp);
+    $('#temp').text(res_weather.main.temp + '\u00B0' + 'C');
 
-    let res_nbp_1 = getData(url_nbp_1);
-    let r1 = res_nbp_1.rates;
-
-    let res_nbp_2 = getData(url_nbp_2);
-    let r2 = res_nbp_2.rates;
-
-    let labels_1 = [];
-    let labels_2 = [];
-
-    let dataset_1 = {
-        label: 'GBP rates',
-        data: [],
-        fill: false,
-        backgroundColor: '#ffd866',
-        borderColor: '#8a7c34',
-        borderWidth: 1
-    };
-
-    let dataset_2 = {
-        label: 'EUR rates',
-        data: [],
-        fill: false,
-        backgroundColor: '#146ccc',
-        borderColor: '#162351',
-        borderWidth: 1
-    };
-
-    $.each(r1, function (index, object) {
-        dataset_1.data.push(object.mid);
-        labels_1.push(object.effectiveDate);
-    });
-
-    $.each(r2, function (index, object) {
-        dataset_2.data.push(object.mid);
-        labels_2.push(object.effectiveDate);
-    });
-
+    // DRAWING NBP CHARTS
     drawChart('bar', 'canvas-chart-1', labels_1,[dataset_1]);
     drawChart('bar', 'canvas-chart-2', labels_2,[dataset_2]);
     drawChart('bar', 'canvas-chart-3', labels_1,[dataset_1, dataset_2]);
-
-    // END OF NBP CHARTS
+    // END OF DRAWING NBP CHARTS
 
 
     // GITHUB JOKES
+    writeJoke(joke_q, joke_a, $('#joke-question'), $('#joke-answer'));
 
-    writeJoke($('#joke-question'), $('#joke-answer'));
-
+    // get new joke on key J press
     $(function() {
        $(window).keypress(function(e) {
            var ev = e || window.event;
            var key = ev.keyCode || ev.which;
-           //do stuff with "key" here...
+
+           $('#joke-question').text('loading question...');
+           $('#joke-answer').text('generating answer...');
+
            if (key == 106) {
-               writeJoke($('#joke-question'), $('#joke-answer'));
+               let res_joke = getData(url_joke);
+               let joke_q = res_joke.setup;
+               let joke_a = res_joke.punchline;
+               writeJoke(joke_q, joke_a, $('#joke-question'), $('#joke-answer'));
            }
        });
     });
-
     // END OF GITHUB JOKES
 
 });
